2f38164cb25286f62ceecf70fc430b307b65dda9
FrancivaldoCosta <francivaldoaccosta@gmail.com>
2024-03-14 12:17:44 -0300

    continuando

--- course2/src/application/Program.java
+++ course2/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import javax.xml.crypto.Data;
+
 import entities.Product;
 
 public class Program {
@@ -22,10 +24,25 @@ public class Program {
 		product.quantity = sc.nextInt();
 		
 		
+		System.out.println();
+		System.out.println("product data: " + product );
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
 		
-				
+		System.out.println();
+		System.out.println("Updated data: " + product );
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product );
+		
 		
 
 		sc.close();
