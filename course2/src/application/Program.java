1f55a1a2c9a8fbc78fd39b0abeadae28e9dc439e
Francivaldo Costa <francivaldoaccosta@gmail.com>
2024-03-25 23:17:44 -0300

    Adcionado metodos e classes

--- course2/src/application/Program.java
+++ course2/src/application/Program.java
@@ -15,22 +15,28 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 
+		Product p = new Product();
 		
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		int quantity = sc.nextInt();
-		Product product = new Product(name, price, quantity);
+		Product product = new Product(name, price);
+		
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
+		
 
 		System.out.println();
 		System.out.println("product data: " + product);
 
 		System.out.println();
 		System.out.println("Enter the number of products to be added in stock: ");
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		product.addProducts(quantity);
 
 		System.out.println();
